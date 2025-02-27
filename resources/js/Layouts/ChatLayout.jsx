8bf1d894400e15c734c8bd0e4e15d35046f70495
Kaushal Rangani <kaushalrangani331@gmail.com>
2025-02-27 23:41:36 +0530

    Displayed the conversation/user and group list and make it responsive hour 3 completed but i will say not successfully ( used the deprecated this to get this working )

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,6 +1,9 @@
 import { usePage } from "@inertiajs/react";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import TextInput from "@/Components/TextInput";
+import { PencilSquareIcon } from "@heroicons/react/24/solid";
+import ConversationItem from "@/Components/App/ConversationItem";
 // import Echo from "laravel-echo";
 
 const ChatLayout = ({ children }) => {
@@ -13,45 +16,54 @@ const ChatLayout = ({ children }) => {
 
     const isUserOnline = (userId) => onlineUsers[userId];
 
-    // console.log("conversations", conversations);
-    // console.log("selectedConversation", selectedConversation);
+    console.log("conversations", conversations);
+    console.log("selectedConversation", selectedConversation);
 
-useEffect(() => {
-    setSortedConversations(
-        localConversations.sort((a, b) => {
-            if(a.bloacked_at && b.blocked_at) {
-                return a.blocked_at > b.blocked_at ? -1 : 1;
-            }else if(a.blocked_at) {
-                return 1;
-            }else if(b.blocked_at) {
-                return -1;
-            }
-            if(a.last_message_date && b.last_message_date) {
-                return b.last_message_date.localeCompare(a.last_message_date);
-            }else if(a.last_message_date) {
-                return -1;
-            }else if(b.last_message_date) {
-                return 1;
-            }else {
-                return 0;
-            }
-        })
-    );
-}, [localConversations]);
+    const onSearch = (ev) => {
+        const search = ev.target.value.toLowerCase();
+        setLocalConversations(
+            conversations.filter((conversation) => {
+                return conversation.name.toLowerCase().includes(search);
+            })
+        );
+    }
 
-useEffect(() => {
-    setLocalConversations(conversations);
-}, [conversations]);
+    useEffect(() => {
+        setSortedConversations(
+            localConversations.sort((a, b) => {
+                if (a.bloacked_at && b.blocked_at) {
+                    return a.blocked_at > b.blocked_at ? -1 : 1;
+                } else if (a.blocked_at) {
+                    return 1;
+                } else if (b.blocked_at) {
+                    return -1;
+                }
+                if (a.last_message_date && b.last_message_date) {
+                    return b.last_message_date.localeCompare(a.last_message_date);
+                } else if (a.last_message_date) {
+                    return -1;
+                } else if (b.last_message_date) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+        );
+    }, [localConversations]);
+
+    useEffect(() => {
+        setLocalConversations(conversations);
+    }, [conversations]);
 
     useEffect(() => {
         Echo.join('online')
             .here((users) => {
-                const onlineUsersObj = Object.fromEntries( 
-                    users.map((user) => [user.id, user]) 
-                    ); 
-                setOnlineUsers((prevOnlineUsers) => { 
-                    return { ...prevOnlineUsers, ...onlineUsersObj }; 
-                    }); 
+                const onlineUsersObj = Object.fromEntries(
+                    users.map((user) => [user.id, user])
+                );
+                setOnlineUsers((prevOnlineUsers) => {
+                    return { ...prevOnlineUsers, ...onlineUsersObj };
+                });
             })
             .joining((user) => {
                 setOnlineUsers((prevOnlineUsers) => {
@@ -74,9 +86,44 @@ useEffect(() => {
             Echo.leave('online');
         }
     }, [])
-    return <>
-        ChatLayout
-        <div>{children}</div>
-    </>
+    return (
+        <>
+            <div className="flex-1 w-full flex overflow-hidden">
+                <div className={`transition-all w-full sm:w-[220px] md:w-[300px] bg-slate-800 flex flex-col overflow-hidden ${selectedConversation ? "-ml-[100%] sm:ml-0" : ""}`} >
+                <div className="flex items-center justify-between py-2 px-3 text-xl font-medium text-gray-200">
+                    My Conversations
+                    <div className="tooltip tooltip-left" data-tip="Create new Group">
+                        <button className="text-gray-400 hover:text-gray-200">
+                            <PencilSquareIcon className="w-4 h-4 inline-block ml-2" />
+                        </button>
+                    </div>
+                </div>
+                <div className="p-3">
+                    <TextInput
+                        onKeyUp={onSearch}
+                        placeholder="Filter users and groups"
+                        className="w-full"
+                    />
+                </div>
+                <div className="flex-1 overflow-auto">
+                    {sortedConversations && sortedConversations.map((conversation) => (
+                        <ConversationItem
+                            key={`${
+                                conversation.is_group ? "group_" : "user_"
+                            }${conversation.id}`}
+                            conversation={conversation}
+                            online={!!isUserOnline(conversation.id)}
+                            selectedConversation={selectedConversation}
+                        />  
+                    ))}
+                </div>
+                </div>
+                <div className="flex-1 flex flex-col overflow-hidden">
+                    {children}
+                </div>
+            
+            </div>
+        </>
+    )
 }
 export default ChatLayout;
